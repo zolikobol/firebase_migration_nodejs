dc6ef6ace4b50b2e7365f5908fb4739797715330
zolikobol <zoltan.kobol@globallogic.com>
2017-01-10 14:27:15 +0100

    finished unsubscribe

--- helper.js
+++ helper.js
@@ -1,5 +1,6 @@
 var Converter = require("csvtojson").Converter
     fs = require("fs");
+var Promise = require("promise");
 
 var pairedWithTopics = Array();
 var registration_id = Array();
@@ -28,39 +29,66 @@ Helper.prototype.pairTokenWithRegid = function(input , output , callback){
     console.log("pairing device tokens with registration ID");
     console.log("getting tags for registration ID");
 
-    var len = input.length;
-
-    var paired = [];
-
-    console.log(len);
-
-    for(var i = 0; i< len; i++){
-        element = input[i]["reg_id"];
-        found = this.findElementParams(input , output, element , i);
-        if(found != null){
-            paired.push(found);
-        }
-    }
-
-    var pairedArray = paired.map(function(obj){
-        var len = obj.tags.length;
-        for(var i = 0; i < len; i++){
-            if(!Array.isArray(pairedWithTopics[obj.tags[i]])){
-                pairedWithTopics[obj.tags[i]] = [];
-            }
-            pairedWithTopics[obj.tags[i]].push(obj.registration_token);
-        }
-        registration_id.push(obj);
+    var elements = findElements(input , output);
+    elements.then(data => {
+        var pairedElements = pairDevices(data);
+        pairedElements.then(newData => {
+            //console.log(newData["registration_id"]);
+            callback(newData["paired"] , newData["registration_id"]);
+        })
+        
     })
-    callback(pairedWithTopics , registration_id);
+    elements.catch(error => { 
+        console.log(error) 
+    });
+
 }
 
-Helper.prototype.findElementParams = function(in_csv, out_csv , element , input_position){
+function pairDevices(paired){
+    return new Promise(function(resolve , reject){
+        var pairedArray = paired.map(function(obj){
+            var len = obj.tags.length;
+            for(var i = 0; i < len; i++){
+                if(!Array.isArray(pairedWithTopics[obj.tags[i]])){
+                    pairedWithTopics[obj.tags[i]] = [];
+                }
+                pairedWithTopics[obj.tags[i]].push(obj.registration_token);
+            }
+            registration_id.push(obj);
+        })
+
+        resolve(
+            {
+                paired : pairedWithTopics , 
+                registration_id : registration_id
+            }
+            )
+    })
+}
+
+function findElements(input , output){
+    return new Promise(function(resolve , reject){
+        //console.log(input);
+        var len = input.length;
+        var paired = [];
+        for(var i = 0; i< len; i++){
+            element = input[i]["Registration Id"];
+            found = findElementParams(input , output, element , i);
+            if(found != null){
+                //console.log(i + " device paired");
+                paired.push(found);
+            } else {
+                reject("not paired");
+            }
+        } 
+        resolve(paired);
+    })
+}
+
+function findElementParams(in_csv, out_csv , element , input_position){
 
     var len = out_csv.length;
     var found = {};
-
-    //console.log(element);
 
     for(var i = 0; i < len; i++){
         //console.log(out_csv[i]["apns_token"]);
@@ -77,5 +105,7 @@ Helper.prototype.findElementParams = function(in_csv, out_csv , element , input_
     }
 
 }
+
+
 
 module.exports = Helper;
--- index.js
+++ index.js
@@ -5,18 +5,26 @@ var Promise = require("promise");
 var asynch = require("asynch");
 var rp = require('request-promise');
 
+var parameters = process.argv.slice(2);
+
+var key = parameters[0];
+var inCsv = parameters[1];
+var outCsv = parameters[2];
+
 var helper = new Helper();
+
+//AIzaSyA18DOy5YTyPoUFK8x6hASacSL3xH68upc
 
 var json1, json2;
 
 var inCsv = new Promise((resolve , reject) => {
-    helper.readCsvFile("./csv/com.economist.ipad.in.csv" , function(csvContent){  
+    helper.readCsvFile("./csv/" + inCsv , function(csvContent){  
         resolve(csvContent);
     })
 });
 
 var outCsv =  new Promise((resolve , reject) => {
-    helper.readCsvFile("./csv/com.economist.ipad.in_out.csv" , function(csvContent){
+    helper.readCsvFile("./csv/" + outCsv , function(csvContent){
         resolve(csvContent);
     })
 })
@@ -26,15 +34,72 @@ Promise.all([inCsv, outCsv]).then(values => {
     json2 = values[1];
     console.log("input and output files read");
     helper.pairTokenWithRegid(json1 , json2 , function(result , regID){
-        console.log("paired");
-        var unsubscribed = unsubscribeFromTopic(regID);
-        unsubscribed.then(function(result){
-            console.log("aa");
-        }).catch(function(err){
-            console.log("error");
-        });
+        //var unsubscribed = unsubscribeFromTopic(regID);
+        //console.log(typeof result)
+        //console.log(typeof regID)
+        subscribeToTopic(result);
   })
 });
+
+function subscribeToTopic(data){
+
+    Object.keys(data).map(function(key, index) {
+        subscribe(key , data[key] , topic => {
+            if(topic != null){
+                console.log('devices to topic ' + topic + ' were subscribed')
+            }
+        })
+    });
+
+    
+    /*var result = Promise.all(data.map(function(items){
+        return new Promise(function(resolve, reject){
+            resolve(items);
+        })
+    })).then(function(res){
+        console.log(res);
+    }).catch(function(err){
+        console.log(err)
+    })*/
+
+}
+
+function subscribe(topic , data , callback ){
+    
+    while(data.length > 0){
+        chunk = data.slice(0 , 98);
+        var options = {
+            uri: 'https://iid.googleapis.com/iid/v1:batchAdd',
+            method: 'POST',
+            headers: {
+                'Authorization' : "key=" + key,
+        
+            },
+            body: {
+                "to": "/topics/" + topic,
+                "registration_tokens": chunk
+            },
+            json: true,
+        };
+
+        console.log(options);
+
+        /*rp(options)
+            .then(function(res){
+                delete chunk;
+                delete len;
+                console.log(res);
+                console.log("=============================================");
+                callback(topic);
+            }).catch(function(err){
+                delete chunk;
+                delete len;
+                console.log(err)
+            })*/
+
+    }
+    
+}
 
 function unsubscribeFromTopic(regID){
 
@@ -42,57 +107,61 @@ function unsubscribeFromTopic(regID){
 
     var devicesCount = 0;
 
-    var result = new Array();
+    var result = Promise.all(regID.map(function(item){
+        return new Promise(function(resolve,reject){
+            var options = {
+                uri: "https://iid.googleapis.com//iid/info/" + item.registration_token + "?details=true",
+                method: 'GET',
+                headers: {Authorization : "key=" + key}
+            }
 
-    Promise.all(regID.map(function(item){
-    
-        var options = {
-            uri: "https://iid.googleapis.com//iid/info/" + item.registration_token + "?details=true",
-            method: 'GET',
-            headers: {Authorization : "key=AIzaSyA18DOy5YTyPoUFK8x6hASacSL3xH68upc"}
-        }
-
-        rp(options)
-            .then(function(res){
-                if(res.indexOf("<HTML>") === -1){
-                    res = JSON.parse(res);
-                    if(typeof res["rel"] != "undefined"){
-                        var keys = Object.keys(res["rel"]["topics"]);
-                        var oldTopics = getOldTopics(keys);
-                        res["rel"]["topics"] = oldTopics;
-                        keys = res["rel"]["topics"];
-                        var len = keys.length;
-                        result.push({
-                            reg_token: item.registration_token,
-                            tags: keys
-                        })
-                        //console.log(item.registration_token , keys)
-                        /*for(var i = 0; i< len; i++){
-                            unsubscribe(item.registration_token , keys[i]);
-                        }*/
+            rp(options)
+                .then(function(res){
+                    if(res.indexOf("<HTML>") === -1){
+                        res = JSON.parse(res);
+                        if(typeof res["rel"] != "undefined"){
+                            var keys = Object.keys(res["rel"]["topics"]);
+                            var oldTopics = getOldTopics(keys);
+                            res["rel"]["topics"] = oldTopics;
+                            keys = res["rel"]["topics"];
+                            var len = keys.length;
+                            //console.log(keys);
+                            
+                            console.log(item.registration_token , keys)
+                            for(var i = 0; i< len; i++){
+                                unsubscribe(item.registration_token , keys[i]);
+                            }
+                            resolve({
+                                reg_token: item.registration_token,
+                                tags: keys
+                            });
+                        }
                     }
-                }
 
-            })
-            .catch(function(err){
-                //console.log(err);
-            })
-    })).then(function(res){
-        console.log(result);
-    });
+                })
+                .catch(function(err){
+                    reject(err)
+                })
+        });
+        
+    })).then(values => {
+        console.log(values);
+    })
+
+    result.then(function(data){
+        //console.log(data);
+    })
 }
-
-//unsubscribe('drRkOuztyGY:APA91bE6iwtHMBQ74CnxIg0Ik1-R35YRmTxYX1Nx-WUt7ITS5Drbu7tTbH7xdfOwTnIJW2tir4y59Lng0yCS7jhlpk6JqzlrutowtlVozfq69VsXahW-HNTlBUJ8hzI1N_vjwuAy5TSd' , 'Device_Type_iPad');
 
 function unsubscribe(regID , topic){
 
-    console.log(regID , topic);
+    //console.log(regID , topic);
 
     var options = {
         uri: 'https://iid.googleapis.com/iid/v1:batchRemove',
         method: 'POST',
         headers: {
-            'Authorization' : "key=AIzaSyA18DOy5YTyPoUFK8x6hASacSL3xH68upc",
+            'Authorization' : "key=" + key,
     
         },
         body: {
@@ -104,9 +173,11 @@ function unsubscribe(regID , topic){
 
     rp(options)
         .then(function(res){
-            console.log(res)
+            console.log(regID + " - ");
+            console.log(res);
+            console.log("====================");
         }).catch(function(err){
-            //console.log(err)
+            console.log(err)
         })
 }
 
